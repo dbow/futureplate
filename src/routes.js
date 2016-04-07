dd45e43b693479a1bdc1feb70e3d9860e2401415
Danny Bowman <danny@d-bow.com>
2016-04-07 00:18:06 -0400

    Remove unused lodash imports

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 
--- src/stores/cache.js
+++ src/stores/cache.js
@@ -1,6 +1,3 @@
-import _ from 'lodash';
-
-
 export default class Cache {
   constructor() {
     this.cache = {};
